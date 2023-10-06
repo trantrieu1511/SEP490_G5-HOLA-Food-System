e61998e0ba08d52887f53f960b129b8142ac990b
Duoc Nguyen <duocng.2409@gmail.com>
2023-10-06 21:03:38 +0700

    re create project

--- HFS-BE/HFS-BE/Program.cs
+++ HFS-BE/HFS-BE/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
