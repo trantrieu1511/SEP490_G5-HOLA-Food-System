2f0040421c3629562d8ca91f418f277bb886ad5e
lunguyenhe <lunthe150983@fpt.edu.vn>
2023-10-09 11:44:31 +0700

    login register

---
+++ HFS-BE/HFS-BE/BusinessLogic/Auth/AuthBusinessLogic.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HFS_BE.Base;
+using HFS_BE.BusinessLogic.Homepage;
+using HFS_BE.DAO.AuthDAO;
+using HFS_BE.DAO.UserDAO;
+using HFS_BE.Models;
+
+namespace HFS_BE.BusinessLogic.Auth
+{
+	public class AuthBusinessLogic : BaseBusinessLogic
+	{
+		public AuthBusinessLogic(SEP490_HFSContext context, IMapper mapper) : base(context, mapper)
+		{
+		}
+		public LoginOutputDto Login(LoginInPutDto inputDto)
+		{
+			try
+			{
+				var dao = this.CreateDao<AuthDAO>();
+				var daoinput = mapper.Map<LoginInPutDto, AuthInputDto>(inputDto);
+				var daooutput = dao.Login(daoinput);
+				var output = mapper.Map<AuthOutputDto, LoginOutputDto>(daooutput);
+
+				return output;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	
+	}
+}
---
+++ HFS-BE/HFS-BE/DAO/AuthDAO/AuthOutputDto.cs
@@ -0,0 +1,12 @@
+﻿using HFS_BE.Base;
+
+namespace HFS_BE.DAO.AuthDAO
+{
+	public class AuthOutputDto:BaseOutputDto
+	{
+	public	string Token { get; set; }
+	}
+
+	
+
+}
