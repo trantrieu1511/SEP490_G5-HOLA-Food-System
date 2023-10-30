6077ad49333106b0fc7a5fbd732b69b3669dc076
lunguyenhe <lunthe150983@fpt.edu.vn>
2023-10-30 20:51:27 +0700

    login ok register ok
    done

--- HFS-BE/HFS-BE/Controllers/Auth/LoginController.cs
+++ HFS-BE/HFS-BE/Controllers/Auth/LoginController.cs
@@ -28,19 +28,20 @@ namespace HFS_BE.Controllers.Auth
 				throw;
 			}
 		}
-		[HttpPost("home/loginseller")]
-		public LoginOutputDto LoginSeller(LoginInPutDto inputDto)
+		[HttpPost("home/loginnotcustomer")]
+		public LoginOutputDto LoginNotCustomer(LoginInPutDto inputDto)
 		{
 			try
 			{
-				var business = this.GetBusinessLogic<AuthBusinessLogic>();
+				var business = this.GetBusinessLogic<AuthNotCustomerBusinessLogin>();
 
-				return business.L(inputDto);
+				return business.LoginNotCustomer(inputDto);
 			}
 			catch (Exception ex)
 			{
 				throw;
 			}
 		}
+		
 	}
 }
