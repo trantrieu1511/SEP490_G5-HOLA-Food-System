89f91d5980b06c6fa20757301fabce2edea4f012
Duoc Nguyen <duocng.2409@gmail.com>
2023-10-31 21:01:34 +0700

    submit get feedback

---
+++ HFS-BE/HFS-BE/Controllers/FoodDetail/GetFeedBackController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HFS_BE.Base;
+using HFS_BE.BusinessLogic.FoodDetail;
+using HFS_BE.DAO.FeedBackDao;
+using HFS_BE.Models;
+using HFS_BE.Utils;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HFS_BE.Controllers.FoodDetail
+{
+    public class GetFeedBackController : BaseController
+    {
+        public GetFeedBackController(SEP490_HFS_2Context context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        [HttpPost("fooddetail/feedbck")]
+        public GetFeedBackOutputDto GetFeedBack(GetFeedBackInputDto inputDto)
+        {
+            try
+            {
+                var userInfor = this.GetUserInfor();
+                inputDto.CustomerId = userInfor != null ? userInfor.UserId : null;
+                var busi = this.GetBusinessLogic<GetFeedBackBusinessLogic>();
+                return busi.GetFeedBack(inputDto);
+            }
+            catch (Exception)
+            {
+                return this.Output<GetFeedBackOutputDto>(Constants.ResultCdFail);
+            }
+        }
+    }
+}
