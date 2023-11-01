9dbdbb81950f00c37305b7ee7ce9b0fb5c82d30c
trantrieu1511 <trantrieu15112k2@gmail.com>
2023-11-01 15:09:05 +0700

    Update FE of manage profile

--- HFS-FE/src/app/modules/customer-routing-module/components/manageprofile/manageprofile.component.ts
+++ HFS-FE/src/app/modules/customer-routing-module/components/manageprofile/manageprofile.component.ts
@@ -52,6 +52,7 @@ export class ManageprofileComponent extends iComponentBase implements OnInit {
 
   async getProfileInfo() {
     try {
+      debugger;
       const params = {
         userId: sessionStorage.getItem("userId")
         // userId: 1
