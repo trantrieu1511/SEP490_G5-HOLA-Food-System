f6220a3dd9db59fd6b6fca5c09c4cd5b7d725ce3
lunguyenhe <lunthe150983@fpt.edu.vn>
2023-11-02 17:24:58 +0700

    done moderator
    ok

--- HFS-BE/HFS-BE/BusinessLogic/ManageUser/CreateModerator.cs
+++ HFS-BE/HFS-BE/BusinessLogic/ManageUser/CreateModerator.cs
@@ -6,14 +6,14 @@ namespace HFS_BE.BusinessLogic.ManageUser
 	{
 		
 
-			public string FirstName { get; set; } = null!;
-			public string LastName { get; set; } = null!;
+			public string FirstName { get; set; } 
+			public string LastName { get; set; } 
 			public string? Gender { get; set; }
 			public DateTime? BirthDate { get; set; }
 		    public string? PhoneNumber { get; set; }
-		    public string Email { get; set; } = null!;
-			public string Password { get; set; } = null!;
-		    public string CofirmPassword { get; set; } = null!;
+		    public string Email { get; set; } 
+	     	public string Password { get; set; }
+		    public string ConfirmPassword { get; set; }
 
 	}
 }
