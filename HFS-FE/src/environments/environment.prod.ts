631db884f90b3ae34c6b4f8a388958bfbcbee303
Anh Pham <phamanhqb73@gmail.com>
2023-11-20 16:12:32 +0700

    test deploy

--- HFS-FE/src/environments/environment.prod.ts
+++ HFS-FE/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
-import { environment as common } from './environment';
-
 export const environment = {
-  ...common,
+  apiUrl: "https://localhost:7016/",
+  hubUrl: "https://localhost:7016/hubs/",
+  clientId: "737110996158-747oghg2rqifouoldnkonnp2vfp174qh.apps.googleusercontent.com",
+
+  appId: "297572859708452",
   production: true
 };
