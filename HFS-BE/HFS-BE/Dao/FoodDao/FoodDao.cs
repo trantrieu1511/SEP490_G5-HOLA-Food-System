125e6a1d071fec82b37b699304a834c277088230
Duoc Nguyen <duocng.2409@gmail.com>
2023-11-29 12:06:48 +0700

    Update FoodDao.cs

--- HFS-BE/HFS-BE/Dao/FoodDao/FoodDao.cs
+++ HFS-BE/HFS-BE/Dao/FoodDao/FoodDao.cs
@@ -92,10 +92,36 @@ namespace HFS_BE.Dao.FoodDao
                     .Where(x => x.FoodId == inputDto.FoodId && x.Status == 1)
                     .FirstOrDefault();
 
+                if (data == null)
+                {
+                    return this.Output<FoodOutputDto>(Constants.ResultCdFail);
+                }
+
                 var shopid = data.SellerId;
 
                 var output = this.Output<FoodOutputDto>(Constants.ResultCdSuccess);
                 output = mapper.Map<Food, FoodOutputDto>(data);
+
+                int ordered = 0;
+                decimal star = 0;
+                foreach (var orderdetail in data.OrderDetails)
+                {
+                    ordered += orderdetail.Quantity.Value;
+                }
+
+                if (data.Feedbacks.Any())
+                {
+                    int totalStar = 0;
+                    foreach (var feedback in data.Feedbacks)
+                    {
+                        totalStar += feedback.Star.Value;
+                    }
+                    star = (decimal)totalStar / (decimal)data.Feedbacks.Count();
+                }
+
+                output.AverageStar = Math.Round(star, MidpointRounding.AwayFromZero);
+                output.NumberOrdered = ordered;
+
                 foreach (var img in output.foodImages)
                 {
                     var imageInfor = ImageFileConvert.ConvertFileToBase64(shopid, img.Name, 1);
