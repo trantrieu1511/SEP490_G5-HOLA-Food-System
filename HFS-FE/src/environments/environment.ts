a5efb05161df115a3681ebb237541faa1eed67a6
hola <holafood@instance-2.c.mayao-406906.internal>
2023-12-06 08:42:58 +0000

    test

--- HFS-FE/src/environments/environment.ts
+++ HFS-FE/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: "https://localhost:7016/",
+  apiUrl: "/api/",
   clientId: "737110996158-747oghg2rqifouoldnkonnp2vfp174qh.apps.googleusercontent.com",
 
   appId: "297572859708452",
