3664efd459d61f29057462de72b72b401baa2277
hola <holafood@instance-2.c.mayao-406906.internal>
2023-12-07 05:33:14 +0000

    add base deploy 1

--- HFS-FE/src/environments/environment.prod.ts
+++ HFS-FE/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  apiUrl: "/api/",
+  clientId: "737110996158-747oghg2rqifouoldnkonnp2vfp174qh.apps.googleusercontent.com",
+
+  appId: "297572859708452",
   production: true
 };
