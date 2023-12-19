21d546a853fa226af1b31c12f1d0bd2d58b04adc
trantrieu1511 <trantrieu15112k2@gmail.com>
2023-12-19 21:51:48 +0700

    Sua profile page, sua luon cho a Lu

--- HFS-FE/src/app/register/register.component.ts
+++ HFS-FE/src/app/register/register.component.ts
@@ -547,7 +547,7 @@ printResult() {
   const formattedDay = day < 10 ? '0' + day : day;
   const formattedMonth = month < 10 ? '0' + month : month;
 
-  return formattedDay + '/' + formattedMonth + '/' + year;
+  return year + '/' + formattedMonth + '/' + formattedDay;
 }
 }
 interface Role {
